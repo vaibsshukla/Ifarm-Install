e885ff90162e817520e08f6452466ad2674843d8
Enlightment <B3DS-D01@users.noreply.github.com>
2019-01-30 12:15:06 +0530

    Add files via upload

---
+++ src/main/java/com/b3ds/ifarm/installation/App.java
@@ -0,0 +1,13 @@
+package com.b3ds.ifarm.installation;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class App {
+
+	private final static Logger logger = LogManager.getLogger(App.class);
+
+}
